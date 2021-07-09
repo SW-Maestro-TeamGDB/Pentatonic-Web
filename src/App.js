4fae066f89c469983dc5b26f851ba8c8d9382cc0
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-07-09 15:53:57 +0900

    feat: apollo client 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { createHttpLink } from 'apollo-link-http';
+
+const client = new ApolloClient({
+  link: createHttpLink({
+    uri: 'https://pentatonic.cdn.ntruss.com/api',
+    // headers: {
+    //   'Content-Type': 'application/json',
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Credentials': true,
+    // },
+    // fetch,
+    fetchOptions: { mode: 'no-cors' },
+  }),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ApolloProvider client={client}></ApolloProvider>;
 }
 
 export default App;
