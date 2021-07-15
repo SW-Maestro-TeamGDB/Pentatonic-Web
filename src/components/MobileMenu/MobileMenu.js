302273b4ae7513060968fa75e40c25e2602af723
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-07-15 16:42:17 +0900

    feat: 모바일 상세 메뉴 추가

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -9,13 +9,24 @@ const MobileMenu = (props) => {
       <MenuBackGround>
         <Divider />
         <MenuLink to="/lounge">라운지</MenuLink>
-        <MenuLink to="/studio">스튜디오</MenuLink>
-        <MenuLink to="/artist">아티스트</MenuLink>
+        <SubMenuLink to="/lounge/weekly">위클리 챌린지</SubMenuLink>
+        <SubMenuLink to="/lounge/band">떠오르는 밴드커버</SubMenuLink>
+        <SubMenuLink to="/lounge/solo">떠오르는 솔로커버</SubMenuLink>
         <Divider />
-        <MenuLink to="/profile">마이페이지</MenuLink>
-        <MenuLink to="/liked">좋아요 누른 커버</MenuLink>
-        <MenuLink to="/library">라이브러리</MenuLink>
-        <MenuLink to="/">로그아웃</MenuLink>
+        <MenuLink to="/studio">스튜디오</MenuLink>
+        <SubMenuLink to="/studio/band">밴드 커버</SubMenuLink>
+        <SubMenuLink to="/studio/solo">솔로 커버</SubMenuLink>
+        <Divider />
+        <MenuLink to="/artist">아티스트</MenuLink>
+        <SubMenuLink to="/artist/rising/band">라이징 밴드</SubMenuLink>
+        <SubMenuLink to="/artist/rising/solo">라이징 아티스트</SubMenuLink>
+        <SubMenuLink to="/artist/rank/band">밴드 랭킹</SubMenuLink>
+        <SubMenuLink to="/artist/rank/solo">아티스트 랭킹</SubMenuLink>
+        <Divider />
+        <SubMenuLink to="/profile">마이페이지</SubMenuLink>
+        <SubMenuLink to="/liked">좋아요 누른 커버</SubMenuLink>
+        <SubMenuLink to="/library">라이브러리</SubMenuLink>
+        <SubMenuLink to="/">로그아웃</SubMenuLink>
       </MenuBackGround>
     </MenuContainer>
   );
@@ -53,8 +64,15 @@ const MenuLink = styled(Link)`
   color: black;
   margin: 1vh 0;
   text-align: center;
-  font-weight: 400;
+  font-weight: 600;
+  font-size: 1.1rem;
+`;
+
+const SubMenuLink = styled(Link)`
+  color: black;
   font-size: 0.8rem;
+  font-weight: 500;
+  margin: 0.7vh 1rem;
 `;
 
 export default MobileMenu;
