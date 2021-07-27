0364070ecec27108f32e4150f0e93d980a94edd0
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-07-27 23:39:16 +0900

    feat: 음성 녹음 및 파일 저장

--- src/pages/Studio/Studio.js
+++ src/pages/Studio/Studio.js
@@ -6,6 +6,7 @@ import StudioBandCover from '../StudioBandCover';
 import StudioSoloCover from '../StudioSoloCover';
 import MusicInformation from '../MusicInformation';
 import CoverMaking from '../CoverMaking';
+import RecordPage from '../RecordPage';
 
 const Studio = ({ match }) => {
   return (
@@ -14,17 +15,10 @@ const Studio = ({ match }) => {
       <Route exact path={match.path} component={StudioHome} />
       <Route path={`${match.path}/band`} component={StudioBandCover} exact />
       <Route path={`${match.path}/solo`} component={StudioSoloCover} exact />
-      <Route
-        path={`${match.path}/band/:id`}
-        component={MusicInformation}
-        exact
-      />
-      <Route
-        path={`${match.path}/solo/:id`}
-        component={MusicInformation}
-        exact
-      />
-      <Route path={`${match.path}/cover/:id`} component={CoverMaking} />
+      <Route path={`${match.path}/:id`} component={MusicInformation} exact />
+
+      <Route path={`${match.path}/:id/cover`} component={CoverMaking} exact />
+      <Route path={`${match.path}/:id/record`} component={RecordPage} exact />
     </>
   );
 };
