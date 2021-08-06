9429326905f66c3c690cb31d5797664c738240d8
Jongminfire <jongminfire@gmail.com>
2021-08-06 18:26:44 +0900

    feat: 녹음 페이지 모달 내용 추가

--- src/components/RecordModal/RecordModal.js
+++ src/components/RecordModal/RecordModal.js
@@ -5,6 +5,8 @@ import LoginModal from '../LoginModal';
 import FindAccountModal from '../FindAccoutModal/FindAccountModal';
 import FindIdModal from '../FindIdModal.js/FindIdModal';
 import FindPasswordModal from '../FindPasswordModal';
+import MicAuth from './MicAuth.png';
+import SilenceIcon from './SilenceIcon.png';
 
 const RecordModal = (props) => {
   const { modalToggle, setModalToggle } = props;
@@ -21,6 +23,17 @@ const RecordModal = (props) => {
       centered
     >
       <ModalTitle>녹음 시 주의사항</ModalTitle>
+      <CautionContainer>
+        <CautionContent>
+          <CautionTitle>1. 마이크 권한을 허용해주세요</CautionTitle>
+          <CustomImg src={MicAuth} width="50%" />
+          <Spacing />
+          <CautionTitle>2. 녹음은 조용한 곳에서 진행해주세요</CautionTitle>
+          <CustomImg src={SilenceIcon} width="20%" />
+          <Spacing />
+          <SubmitButton onClick={() => closeModal()}>확인했습니다</SubmitButton>
+        </CautionContent>
+      </CautionContainer>
     </CustomModal>
   );
 };
@@ -30,10 +43,60 @@ const CustomModal = styled(Modal)`
   overflow: hidden;
 `;
 
+const CustomImg = styled.img`
+  margin: 1rem 0;
+`;
+
+const CautionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2.5rem;
+`;
+
+const CautionTitle = styled.p`
+  font-size: 1.2rem;
+  padding: 0.5rem 1rem;
+  color: black;
+  font-weight: 800;
+  width: 100%;
+  border-radius: 0.5rem;
+`;
+
+const CautionContent = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 const ModalTitle = styled.div`
-  font-size: 1.5rem;
+  font-size: 1.8rem;
   font-weight: 800;
   text-align: center;
+`;
+
+const SubmitButton = styled.button`
+  border-radius: 0.8rem;
+  background-color: black;
+  margin: 1rem 0;
+  width: 95%;
+  height: 4rem;
+  border: none;
+  color: white;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: all ease-in-out 0.3s;
+  &:hover {
+    background-color: rgb(50, 50, 50);
+  }
+`;
+
+const Spacing = styled.div`
+  height: 1.5rem;
+  border-bottom: 1px solid black;
 `;
 
 export default RecordModal;
