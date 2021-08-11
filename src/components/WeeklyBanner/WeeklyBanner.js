862a89fbd6f18203f69be122c6ed6663c0aaa8b5
Jongminfire <jongminfire@gmail.com>
2021-08-11 17:02:39 +0900

    chore: WeeklyBanner hover 애니메이션과 밝기 변화 수정

--- src/components/WeeklyBanner/WeeklyBanner.js
+++ src/components/WeeklyBanner/WeeklyBanner.js
@@ -27,12 +27,12 @@ const Background = styled.div`
   background-image: url('https://w.namu.la/s/8f16d9ad8ac378b6d2339ce927bbc9d6431dbf5277b241bf363ffa61cf5496caf1611f471aca282ff14bd8e544135b8f5edbbfebb6f942603cc9563f130a548cf40005956d405598ed3f6067522ad7b6aaf067e05dbc1e79085d5b90fb90ab5f9947a0cd3108efda6f8008666a1627cc');
   background-repeat: no-repeat;
   background-position: top center;
-  background-size: 100%;
+  background-size: cover;
   border-radius: 15px;
+  transition: all 0.3s ease-in-out;
 
   &:hover {
-    background-size: 115%;
-    transition: all ease 0.5s;
+    transform: scale(1.1);
   }
 `;
 
@@ -44,9 +44,14 @@ const BannerContainer = styled.div`
   border-radius: 10px;
   position: relative;
   cursor: pointer;
+  overflow: hidden;
 
   ${Background} {
     filter: brightness(50%);
+
+    &:hover {
+      filter: brightness(70%);
+    }
   }
 `;
 
