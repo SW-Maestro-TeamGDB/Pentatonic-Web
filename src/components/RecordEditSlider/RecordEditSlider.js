8dc06500067f95dec62f62c5e94281c854e3c567
Jongminfire <jongminfire@gmail.com>
2021-08-13 21:02:20 +0900

    feat: 음원 후 처리 슬라이더 컴포넌트 구현

---
+++ src/components/RecordEditSlider/RecordEditSlider.js
@@ -0,0 +1,115 @@
+import react from 'react';
+import styled from 'styled-components';
+import { media } from '../../lib/Media';
+import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
+import { Slider } from 'antd';
+
+const RecordEditSlider = (props) => {
+  const { value, setValue, title, desc, max, min } = props;
+
+  const onChangeValue = (value) => {
+    setValue(value);
+  };
+
+  const onClickPlus = () => {
+    if (value + 1 <= max) {
+      setValue((value) => value + 1);
+    }
+  };
+
+  const onClickMinus = () => {
+    if (value - 1 >= min) {
+      setValue((value) => value - 1);
+    }
+  };
+
+  return (
+    <Container>
+      <SliderContainer>
+        <SliderHeader>
+          <SliderTitle>{title}</SliderTitle>
+          <SliderValue>{desc}</SliderValue>
+        </SliderHeader>
+        <SliderWrapper>
+          <SliderMinusIcon onClick={onClickMinus} />
+          <CustomSlider
+            value={value}
+            onChange={onChangeValue}
+            max={max}
+            min={min}
+            tipFormatter={(value) => `${desc}`}
+          />
+          <SliderPlusIcon onClick={onClickPlus} />
+        </SliderWrapper>
+      </SliderContainer>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 90%;
+`;
+
+const SliderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const SliderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+  justify-content: center;
+  width: 100%;
+  margin-top: 1rem;
+`;
+
+const CustomSlider = styled(Slider)`
+  width: 80%;
+`;
+
+const SliderPlusIcon = styled(PlusOutlined)`
+  width: 10%;
+  cursor: pointer;
+  color: gray;
+  transition: all 0.3s ease-in-out;
+  font-size: 1.5em;
+
+  &:hover {
+    color: #000000;
+  }
+`;
+
+const SliderMinusIcon = styled(MinusOutlined)`
+  width: 10%;
+  cursor: pointer;
+  color: gray;
+  transition: all 0.3s ease-in-out;
+  font-size: 1.5em;
+  &:hover {
+    color: #000000;
+  }
+`;
+
+const SliderHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 95%;
+`;
+
+const SliderTitle = styled.div`
+  font-size: 1.2rem;
+  font-weight: 800;
+`;
+
+const SliderValue = styled.div`
+  font-size: 1rem;
+  font-weight: 700;
+`;
+
+export default RecordEditSlider;
