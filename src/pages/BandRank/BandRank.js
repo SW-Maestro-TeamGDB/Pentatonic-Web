7d1718bf64a81622fd726888d76740469d51795d
Jongminfire <jongminfire@gmail.com>
2021-09-04 22:24:49 +0900

    feat: 밴드 랭킹 페이지 구성

--- src/pages/BandRank/BandRank.js
+++ src/pages/BandRank/BandRank.js
@@ -1,27 +1,91 @@
 import react, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import PageContainer from '../../components/PageContainer';
+import RankList from '../../components/RankList';
+
+// tempData
+import TameImpala from '../../images/TempData/TameImpala.jpeg';
+import Hyukoh from '../../images/TempData/Hyukoh.jpeg';
+import Beatles from '../../images/TempData/Beatles.jpeg';
+import MenITrust from '../../images/TempData/MenITrust.jpeg';
+import NoSurprises from '../../images/TempData/NoSurprises.jpeg';
+import TheVolunteers from '../../images/TempData/TheVolunteers.jpeg';
 
 const BandRank = () => {
+  const tempBand = [
+    {
+      backgroundURI: Hyukoh,
+      name: '3인 혁오',
+      likeCount: '730',
+      bandId: 2,
+    },
+    {
+      backgroundURI: NoSurprises,
+      name: '코리아 톰 요크',
+      likeCount: '530',
+      bandId: 5,
+    },
+    {
+      backgroundURI: TheVolunteers,
+      name: '실력은 필요없어',
+      likeCount: '340',
+      bandId: 6,
+    },
+    {
+      backgroundURI: TameImpala,
+      name: '사이키델릭',
+      likeCount: '130',
+      bandId: 1,
+    },
+  ];
+
+  const repeatArr = (arr, num) => {
+    let temp = [];
+
+    for (let i = 0; i < num; i++) {
+      temp = temp.concat(arr);
+    }
+
+    return temp;
+  };
+
+  const showBandRank = () => {
+    return repeatArr(tempBand, 5).map((v, i) => {
+      return (
+        <RankList key={`tempBand+${i}`} data={v} type={'band'} rank={i + 1} />
+      );
+    });
+  };
+
   return (
-    <PageContainer>
-      <h3>밴드 랭킹</h3>
+    <PageContainer width="40%">
+      <PageTitle>밴드 랭킹</PageTitle>
+      <PageDesc>좋아요를 가장 많이 받은 밴드는 누굴까요?</PageDesc>
+      <RankItem>{showBandRank()}</RankItem>
     </PageContainer>
   );
 };
 
-const MenuContainer = styled.div`
+const PageTitle = styled.div`
+  font-size: 2rem;
+  font-weight: 800;
+  margin-top: 1rem;
+`;
+
+const PageDesc = styled.div`
+  font-size: 1rem;
+  margin-top: 1rem;
+`;
+
+const RankItem = styled.div`
+  margin-top: 3.5rem;
   display: flex;
   flex-direction: column;
-  z-index: 2;
-  color: black;
-  background-color: white;
   align-items: center;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
-  padding: 1.5vh 1vw;
-  height: 70vh;
-  width: 70%;
-  margin-top: 3vh;
+  justify-content: center;
+
+  width: 100%;
 `;
 
 export default BandRank;
