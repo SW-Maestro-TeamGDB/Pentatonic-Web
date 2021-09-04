20210e7d6c5c69119b55cfcb064cabf045729a40
Jongminfire <jongminfire@gmail.com>
2021-09-04 22:44:44 +0900

    feat: Lounge,Studio 페이지에 404 페이지 추가

--- src/pages/Lounge/Lounge.js
+++ src/pages/Lounge/Lounge.js
@@ -1,21 +1,23 @@
 import react from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 import LoungeHome from '../LoungeHome';
 import WeeklyChallenge from '../WeeklyChallenge';
 import LoungeBandCovers from '../LoungeBandCovers';
 import LoungeSoloCovers from '../LoungeSoloCovers';
 import CoverRoom from '../CoverRoom/CoverRoom';
+import NotFoundPage from '../NotFoundPage';
 
 const Lounge = ({ match }) => {
   return (
-    <>
+    <Switch>
       <Route exact path={match.path} component={LoungeHome} />
       <Route path={`${match.path}/weekly`} component={WeeklyChallenge} />
       <Route path={`${match.path}/band`} component={LoungeBandCovers} />
       <Route path={`${match.path}/solo`} component={LoungeSoloCovers} />
       <Route path={`${match.path}/cover/:id`} component={CoverRoom} exact />
-    </>
+      <Route path="*" component={NotFoundPage} exact />
+    </Switch>
   );
 };
 
--- src/pages/Studio/Studio.js
+++ src/pages/Studio/Studio.js
@@ -1,5 +1,5 @@
 import react from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import StudioHome from '../StudioHome';
 import StudioBandCover from '../StudioBandCover';
 import StudioSoloCover from '../StudioSoloCover';
@@ -7,29 +7,46 @@ import MusicInformation from '../MusicInformation';
 import CoverMaking from '../CoverMaking';
 import RecordPage from '../RecordPage';
 import RecordMaking from '../RecordMaking/RecordMaking';
+import NotFoundPage from '../NotFoundPage';
 
 const Studio = ({ match }) => {
   return (
-    <>
+    <Switch>
       <Route exact path={match.path} component={StudioHome} />
       <Route path={`${match.path}/band`} component={StudioBandCover} exact />
       <Route path={`${match.path}/solo`} component={StudioSoloCover} exact />
       <Route
-        path={`${match.path}/:sort/:id`}
+        path={`${match.path}/band/:id`}
         component={MusicInformation}
         exact
       />
       <Route
-        path={`${match.path}/:sort/:id/cover`}
+        path={`${match.path}/solo/:id`}
+        component={MusicInformation}
+        exact
+      />
+      <Route
+        path={`${match.path}/band/:id/cover`}
         component={CoverMaking}
         exact
       />
       <Route
-        path={`${match.path}/:sort/:id/record`}
+        path={`${match.path}/solo/:id/cover`}
+        component={CoverMaking}
+        exact
+      />
+      <Route
+        path={`${match.path}/band/:id/record`}
         component={RecordMaking}
         exact
       />
-    </>
+      <Route
+        path={`${match.path}/solo/:id/record`}
+        component={RecordMaking}
+        exact
+      />
+      <Route path="*" component={NotFoundPage} exact />
+    </Switch>
   );
 };
 
