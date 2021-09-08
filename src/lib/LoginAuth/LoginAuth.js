3f078f8955f9ff297b91c1ee33c532dc04543289
Jongminfire <jongminfire@gmail.com>
2021-09-08 18:27:01 +0900

    chore: LoginAuth apollo reactive variables 활용

--- src/lib/LoginAuth/LoginAuth.js
+++ src/lib/LoginAuth/LoginAuth.js
@@ -1,15 +1,21 @@
 import react, { useEffect, useState } from 'react';
 import AuthModal from '../../components/AuthModal';
+import { gql, useQuery } from '@apollo/client';
+import { IS_LOGGED_IN } from '../../apollo/cache';
 import { useHistory } from 'react-router-dom';
 
-const LoginAuth = ({ children, action }) => {
-  const userInfo = localStorage.getItem('userInfo');
-  const token = localStorage.getItem('token');
+const LoginAuth = ({ children }) => {
+  // const userInfo = localStorage.getItem('userInfo');
+  // const token = localStorage.getItem('token');
+  // const isLogged = userInfo && token;
+
+  const { data } = useQuery(IS_LOGGED_IN, {});
 
   const [modalToggle, setModalToggle] = useState(true);
 
-  if (userInfo && token) return <>{children}</>;
-  else
+  if (data?.isLoggedIn) {
+    return <>{children}</>;
+  } else
     return (
       <AuthModal
         modalToggle={modalToggle}
