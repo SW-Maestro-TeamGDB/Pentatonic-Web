c467631fe3d446162a46863b704f2c4e989b164d
Jongminfire <jongminfire@gmail.com>
2021-09-22 20:28:56 +0900

    feat: Google Analytics 연결

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactGA from 'react-ga';
 import reportWebVitals from './reportWebVitals';
+import dotenv from 'dotenv';
 import './App.css';
+
+dotenv.config();
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 
 ReactDOM.render(
   <React.StrictMode>
