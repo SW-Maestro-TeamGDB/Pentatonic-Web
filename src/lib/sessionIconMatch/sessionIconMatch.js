336343cc07ec4817e34ac95e42ae30840f738aef
Jongminfire <jongminfire@gmail.com>
2021-09-28 18:18:36 +0900

    feat: 세션 아이콘 추가 및 sessionIconMatch 함수 연결

--- src/lib/sessionIconMatch/sessionIconMatch.js
+++ src/lib/sessionIconMatch/sessionIconMatch.js
@@ -2,12 +2,24 @@ import drum from '../../images/Session/drum.svg';
 import guitar from '../../images/Session/guitar.svg';
 import piano from '../../images/Session/piano.svg';
 import vocal from '../../images/Session/vocal.svg';
+import bass from '../../images/Session/bass.png';
+import cello from '../../images/Session/cello.png';
+import electric_guitar from '../../images/Session/electric_guitar.png';
+import gayaguem from '../../images/Session/gayaguem.png';
+import haeguem from '../../images/Session/haeguem.png';
+import violin from '../../images/Session/violin.png';
 
 const sessionIconMatch = (position) => {
   if (position === 'ACOUSTIC_GUITAR') return guitar;
   else if (position === 'DRUM') return drum;
   else if (position === 'PIANO') return piano;
   else if (position === 'VOCAL') return vocal;
+  else if (position === 'BASS') return bass;
+  else if (position === 'CELLO') return cello;
+  else if (position === 'ELECTRIC_GUITAR') return electric_guitar;
+  else if (position === 'GAYAGEUM') return gayaguem;
+  else if (position === 'HAEGEUM') return haeguem;
+  else if (position === 'VIOLIN') return violin;
   else return guitar;
 };
 
